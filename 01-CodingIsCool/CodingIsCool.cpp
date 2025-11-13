3bcf2b5db921b38300187d8dcd998da978edfc0f
mafer-solo <aragundi209@gmail.com>
2025-11-13 14:25:48 +0000

    Add files via upload

---
+++ 01-CodingIsCool/CodingIsCool.cpp
@@ -0,0 +1,8 @@
+// This code just give you the output "coding is cool"
+#include<iostream>
+using namespace std;
+
+int main(){ // Main function
+    cout << "Coding is cool" << endl; // Print a single message to the console.
+    return 0; // Return 0 to indicate success.
+}
