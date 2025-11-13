b99d0fa0693c32613c830e07d15f3b6c5adc54fc
mafer-solo <aragundi209@gmail.com>
2025-11-13 21:46:46 +0000

    Delete 01-CodingIsCool directory

--- 01-CodingIsCool/CodingIsCool.cpp
+++
@@ -1,8 +0,0 @@
-// This code just give you the output "coding is cool"
-#include<iostream> // Include iostream library from printing text
-using namespace std; // Allows the use od standard library names (cout, cin, string) without writing std:: before each one
-
-int main(){ // Main function
-    cout << "Coding is cool" << endl; // Print a single message to the console.
-    return 0; // Return 0 to indicate success.
-}
