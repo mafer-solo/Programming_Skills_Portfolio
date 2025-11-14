d0a8faafddb3ab27904b48a1ba8126f57353584b
mafer-solo <aragundi209@gmail.com>
2025-11-14 21:17:53 +0000

    Add files via upload

---
+++ 03- Biography/Biography.cpp
@@ -0,0 +1,38 @@
+// This program stores and prints basic personal information such as name, hometown, and age.
+// Each value is stored in an appropriate. data type. 
+//The output is displayed using a single `cout` statement.
+#include <iostream> // Include iostream library for printing text.
+#include <string> // Allow use of cout without writing std ::count.
+#include <limits> // Allows clearing invalid input and provides min/max limits for data types.
+
+using namespace std;
+
+int main() {
+    string name; // Store the user's full name.
+    string hometown; // Store the user's hometown.
+    int age; // Store the user's age (must be an integer).
+
+    // Input: Full name (with spaces).
+    cout << "Enter your name: " ; // Print the output on the console.
+    getline(cin, name) ; // Read full text with spaces.
+
+    // Input: Hometown ( with spaces)
+    cout << "Enter your hometown: " ; // Print the ourput on the console.
+    getline(cin, hometown) ; 
+
+    // Input: Age with validation.
+    // If the user enter letters, the program asks again.
+    cout << "Enter your age: " ;
+    while (!(cin >> age)) { // If the user enters letters, the program asks again.
+        cout << "Invalid input. Please enter a number: ";
+        cin.clear(); // Clear error state
+    }
+    // Clear the letfover newline to keep input clean for future getline. 
+    cin . ignore(numeric_limits<streamsize>::max(), '\n') ;
+
+    // Output: Only One cout allowed
+    cout << "My name is " << name << "\n" 
+         << "Hometown: " << hometown << "\n"
+         << "Age: " << age << endl;
+    return 0;
+} 
---
+++ 03- Biography/README.md
@@ -0,0 +1,37 @@
+# Biography
+
+## Descrption 
+This program stores, and prints basic personal information such as **name**, **hometown**, and **age**.  
+Each value is stored in a variable.  
+The output of the program depends on the information you stored in the variables.  
+We could also modify the code to ask the user for input, but in this case the values are entered directly into the variables.
+
+## How to run 
+1.- Open the terminal in VS Code.  
+2.- Compile the program:
+
+g++ Biography.cpp -o Biography   
+3.- Execute:
+- On Windows  
+.\Biography.exe  
+- On MacOS/ Linux  
+./Biography
+
+## What I learned
+- How to use `string` and `int` variables.
+- How to use `getline()` for reading strings with spaces, such as full name.
+- How to use `while` loop to continuously ask for valid unput.
+- How to print multiples lines using one `cout` statement.
+- How to manage `numeric and string inputs` together.
+- How to use `cin. ignore()` to clear leftover newline characters after reading integers.
+
+## Academic Integrity Note
+I used ChatGPT to study and understand the task requirements.  
+After learning the concepts, I adapted and wrote the **final version** of the code myself.
+
+## Author
+- **Maria Fernanda Aragundi Ramos** 
+- Conputer Science Student 
+- Bath Spa University.
+
+
